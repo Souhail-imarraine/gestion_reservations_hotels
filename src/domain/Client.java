caa166bd8b006e7a61a7d40b3dd8d97a38ba2f17
imarrainesouhail202@gmail.com <imarrainesouhail202@gmail.com>
2025-09-17 13:05:40 +0200

    Ajout des attributs et des getters/setters de la classe Client

---
+++ src/domain/Client.java
@@ -0,0 +1,36 @@
+package domain;
+
+import java.util.UUID;
+
+public class Client {
+    private UUID id;
+    private String fullName;
+    private String email;
+    private String password;
+
+
+    public Client(String fullName, String email, String password) {
+        this.id = UUID.randomUUID();
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public UUID getId() { return id; }
+    public String getFullName() { return fullName; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+
+    public void setFullName(String fullName)
+    {
+        this.fullName = fullName;
+    }
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+}
